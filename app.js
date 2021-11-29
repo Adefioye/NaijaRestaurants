389878816159a9b1fd973edda289f77fc0372a85
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2021-11-29 09:00:17 +0100

    Added ejs-mate, boilerplate for reusable layout and styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -20,6 +21,7 @@ main()
   });
 
 // setting up templating engine annd path to templates
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
